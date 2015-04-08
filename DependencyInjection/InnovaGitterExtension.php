466983eee0528037628fa05c7a32fb8b165ab6f4
arnaudbey <arnaud.bey@gmail.com>
2015-04-08 16:33:02 +0200

    fix namespace

--- DependencyInjection/InnovaGitterExtension.php
+++ DependencyInjection/InnovaGitterExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Innova\Bundle\GitterBundle\DependencyInjection;
+namespace Innova\GitterBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
